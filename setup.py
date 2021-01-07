dc7060e5324fa80bbc8a2b31f564c69ac2565db1
Yusheng Cai <cys9741@seas.upenn.edu>
2021-01-06 23:40:30 -0500

    changed setup.py from cython compilation to normal python package

--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-import numpy
+from setuptools import setup, find_packages
 
-extensions = [Extension("wham.Bwham",["wham/Bwham.pyx"],\
-            include_dirs=[numpy.get_include()], \
-            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]), \
-            Extension("wham.Uwham",["wham/Uwham.pyx"],\
-             include_dirs=[numpy.get_include()], \
-             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]), \
-            Extension("wham.lib.numeric",["wham/lib/numeric.pyx"],\
-             include_dirs=[numpy.get_include()], \
-             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]), \
-            Extension("wham.lib.utils",["wham/lib/wham_utils.pyx"],\
-             include_dirs=[numpy.get_include()], \
-             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])]
-             
-setup(name='wham',ext_modules = cythonize(extensions,compiler_directives={'language_level' : "3"}))
+
+setup(name='wham',\
+        version='0.0.1',\
+        author='Yusheng Cai',\
+        packages=find_packages(),\
+        install_requires=["numpy","scipy","autograd"])
